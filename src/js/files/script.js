31a3b570020b73ce8257796a5d5fec6b2352d7b7
Andrew Paymushkin <paymushkin_a@bk.ru>
2023-06-26 12:27:01 +0300

    upd

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,14 +3,51 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-let panel = document.querySelector("#panel"),
-	panelBtnOpen = document.querySelector("i.open"),
-	panelBtnClose = document.querySelector("i.close");
+// настройка высоты под устройства
+function setWindowHeight() {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setWindowHeight()
 
-panelBtnOpen.addEventListener("click", ()=> {
-	panel.classList.add("active")
+window.addEventListener("resize", ()=> {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
 })
 
-panelBtnClose.addEventListener("click", ()=> {
-	panel.classList.remove("active")
-})
+// горизонтальный скролл
+const slider = document.querySelectorAll('.scroll-x');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.forEach(element => {
+	element.addEventListener('mousedown', (e) => {
+		isDown = true;
+		element.classList.add('active');
+		startX = e.pageX - element.offsetLeft;
+		scrollLeft = element.scrollLeft;
+	});
+});
+
+slider.forEach(element => {
+	element.addEventListener('mouseleave', () => {
+		isDown = false;
+		element.classList.remove('active');
+	});
+});
+slider.forEach(element => {
+	element.addEventListener('mouseup', () => {
+		isDown = false;
+		element.classList.remove('active');
+	});
+});
+slider.forEach(element => {
+	element.addEventListener('mousemove', (e) => {
+		if (!isDown) return; 
+		e.preventDefault();
+		const x = e.pageX - element.offsetLeft;
+		const walk = (x - startX) * 3;
+		element.scrollLeft = scrollLeft - walk;
+	});
+});
